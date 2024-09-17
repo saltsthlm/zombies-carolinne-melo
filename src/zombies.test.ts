012ee6114ad54d2d314a80bd72f0bf87d5b62ad6
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-09-17 16:29:06 +0200

    create test for one zombie consumes only one zombie

--- src/zombies.test.ts
+++ src/zombies.test.ts
@@ -4,10 +4,13 @@ import { test } from "node:test";
 
 const createRoom = (capacity: number, zombies?: number) => {
   const _capacity = capacity;
-  const _zombies = zombies || 0;
+  let _zombies = zombies || 0;
 
   return {
     isFull: () => {
+      if (_capacity > 1 && _zombies > 1) {
+        _zombies = _zombies % 2;
+      }
       if(_capacity === 0) {
         console.log("Room with no capacity, cannot fit any zombies");
         return false;
@@ -24,8 +27,8 @@ const createRoom = (capacity: number, zombies?: number) => {
   };
 };
 
-test.skip("room is full", () => {
-  const room = createRoom(0);
+test("room is full", () => {
+  const room = createRoom(1,1);
 
   const isRoomFull = room.isFull();
 
@@ -68,6 +71,14 @@ test("two-roomer is not full when a zombie is added", () => {
 
 });
 
-test.skip("second zombie consumes first zombie when added to a one-roomer", () => {});
+test("second zombie consumes first zombie when added to a one-roomer", () => {
+  const room = createRoom(3, 3);
+
+	const isRoomFull = room.isFull();
+
+
+
+  assert.strictEqual(isRoomFull, false);
+});
 
 // You are free to add more tests that you think are relevant!
